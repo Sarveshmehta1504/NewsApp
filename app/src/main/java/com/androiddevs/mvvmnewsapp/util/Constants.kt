e32bad753e7566ce2f815cbef3028bf3508805ad
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2023-12-08 18:55:33 +0530

    News App 1.0

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "97e2be32302b4e8cb1b1d24b650d3d07"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+
+    }
+}
